5ff8cfbde90c8891d8bfa4bdaca72240247e1c2e
abdallahy271 <abdallahy271@gmail.com>
2021-03-14 23:46:27 +0000

    Added Google Maps

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,18 +15,55 @@
 /**
  * Adds a random image to the page.
  */
+let a;
 function randomizeImage() {
+    const location = [
+        {lat:37.422, lng: -122.084},
+        {lat: 39.9514829982124, lng: 32.82864193896349 },
+        {lat:-0.6182972019850904, lng:73.09703007580428 },
+        {lat: 24.470814082528822, lng: 39.617508263244964 },
+        {lat: 36.10039521673435, lng: -112.11208148905413 },
+        {lat: 37.76768251244913, lng: -122.43029199523379 },
+        {lat: 35.06307913148578, lng: 135.74217581612047 },
+        {lat: 41.00549054415733, lng: 28.976631407284994 },
+        {lat: 35.63599072424489, lng:139.63997813974325 },
+        {lat: 6.520748303489362, lng: 3.3560439057592677 },
+        {lat: 35.06307913148578, lng: 135.74217581612047 },
+        {lat: 1.3675398068198992, lng: 103.8737622961487}
+    ];
+
+
   // The images directory contains 11 images, so generate a random index between
   // 1 and 11.
+  
   const imageIndex = Math.floor(Math.random() * 11) + 1;
   const imgUrl = 'images/image-' + imageIndex + '.jpg';
 
   const imgElement = document.createElement('img');
   imgElement.src = imgUrl;
 
+  const mapElement = document.createElement('div');
+  mapElement.setAttribute("id", "map")
+  mapElement.style.height = "400px";
+  
+  const map = new google.maps.Map(
+      mapElement,
+      {center: {lat: location[imageIndex].lat, lng: location[imageIndex].lng }, zoom: 10});
+      
+  const container = document.getElementById('container');
   const imageContainer = document.getElementById('random-image-container');
   // Remove the previous image.
+  if(container.lastElementChild == a){
+    container.removeChild(container.lastElementChild);
+
+}
   imageContainer.innerHTML = '';
   imageContainer.appendChild(imgElement);
+  
+  container.appendChild(mapElement)
+  a = mapElement
+
 }
+
+
 
